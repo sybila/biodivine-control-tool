ec664712963e19924f995496b27017e95f142ab0
MrMooshaber <162307484+MrMooshaber@users.noreply.github.com>
2024-06-17 17:27:39 +0200

    Add files via upload

--- PythonTool/Control_machine.py
+++ PythonTool/Control_machine.py
@@ -1,6 +1,7 @@
 import biodivine_aeon as ba
 from biodivine_aeon import *
 from pathlib import Path
+from Code.GUI import data_processor_GUI
 from Code.Utils import data_processor
 
 """ Program for permanent control over parially specified boolean networks.
@@ -9,27 +10,43 @@ First the inserted phenotype is proccessed, after that you can choose
 from working perturbation the extension of the previous phenotype. """
 
 
-PATH = "C:\\Venca - soubory\\Škola\\Sybila\\Models\\Budding yeast cell cycle (Orlando).aeon"
+PATH = "C:\\Venca - soubory\\Škola\\Sybila\\Models\\Asymmetric Cell Division A no loop.aeon"
+
+# "C:\\Venca - soubory\\Škola\\Sybila\\Models\\Budding yeast cell cycle (Orlando).aeon"
 #"C:\\Venca - soubory\\Škola\\Sybila\\Models\\model_complete_all_general.aeon"
 
-PHENOTYPE = [("CLN3", True)]
-OSCILLATION = False
+PHENOTYPE = [("DnaA", True), ("SciP", True), ("GcrA", False)]
+CONTROLLABLE = ["DnaA", "CcrM", "GcrA"]
+OSCILLATION = "Allowed"
 SIZE = 100
 ROBUSTNESS = 0.01
 
-# [("v_DNA_damage", True)]
-PERTURBABLE = []
-#FILTER_OUT_PARAMETERS = True
-
 #----------------------------------------------------------------
-#def find_unregulated(reg_graph, phenotype_list):
+""" Adds ranking to the perturbations. """
+def add_ranking(working_perts, max_cardinality):
+    evaluated_perts = []
 
+    for perts, card in working_perts:
+        evaluated_perts.append((perts, card, (max_cardinality - card.cardinality()) + len(perts)))
+    
+    evaluated_perts.sort(key = lambda pert: (pert[2], len(pert[0])))
+
+    last_eval = None
+    rank = 0
+
+    for pert_ind in range(len(evaluated_perts)):
+        if (last_eval == None or last_eval != evaluated_perts[pert_ind][2]):
+            last_eval = evaluated_perts[pert_ind][2]
+            rank += 1
+
+        evaluated_perts[pert_ind] = (evaluated_perts[pert_ind][0], evaluated_perts[pert_ind][1], rank)
+
+    return evaluated_perts
 
 """ Converts phenotype_list into the form accepted by the control function. """
 def create_phenotype(model, pert_graph, phenotype_list):
     phenotype = None
     phen_keys = set()
-    unreg_vars = set()
 
     for var_name, fix_val in phenotype_list:
         phen_keys.add(var_name)
@@ -42,7 +59,7 @@ def create_phenotype(model, pert_graph, phenotype_list):
         else:
             phenotype.union(temp_phen)
 
-    return phenotype, phen_keys, unreg_vars
+    return phenotype, phen_keys
 
 """ Finds out if perturbation is subset of another perturbation """
 def perturbation_is_sub(pert, bigger_pert):
@@ -90,12 +107,12 @@ def filter_redundant_data(working_perts, phen_keys):
 def control(model, phenotype_list, oscillation):
     pert_graph = None
 
-    if (len(PERTURBABLE) <= 0):
+    if (len(CONTROLLABLE) <= 0):
         pert_graph = ba.PerturbationGraph(model)
     else:
-        pert_graph = ba.PerturbationGraph.with_restricted_variables(model, perturb=PERTURBABLE)
+        pert_graph = ba.PerturbationGraph.with_restricted_variables(model, perturb=CONTROLLABLE)
 
-    phenotype, phen_keys, unreg_vars = create_phenotype(model, pert_graph, phenotype_list)
+    phenotype, phen_keys = create_phenotype(model, pert_graph, phenotype_list)
 
     control_map = pert_graph.ceiled_phenotype_permanent_control( phenotype = phenotype,
                                                                 size_bound = SIZE if SIZE != None else model.graph().num_vars(),
@@ -106,18 +123,23 @@ def control(model, phenotype_list, oscillation):
     return control_map.working_perturbations(min_robustness=ROBUSTNESS, verbose=False), phen_keys, pert_graph
 
 """ Main function. """
-def control_machine():
-    model_string = Path(PATH).read_text()
+def control_machine(model_path, phenotype, controllable, oscillation, GUI):
+    model_string = Path(model_path).read_text()
     model = ba.BooleanNetwork.from_aeon(model_string)
 
-    phenotype_list = PHENOTYPE
-
-    working_perts, phen_keys, pert_graph = control(model, phenotype_list, "Allowed" if OSCILLATION else "Forbidden")
-
-    working_perts.sort(reverse = True, key = lambda pert: (pert[1].cardinality(), -len(pert[0])))
+    working_perts, phen_keys, pert_graph = control(model, phenotype, oscillation)
 
     working_perts = filter_redundant_data(working_perts, phen_keys)
 
-    data_processor(working_perts, pert_graph.unit_colors().cardinality())
+    max_cardinality = pert_graph.unit_colors().cardinality()
 
-control_machine()
+    working_perts = add_ranking(working_perts, max_cardinality)
+
+    working_perts.sort(reverse = True, key = lambda pert: (pert[1].cardinality(), -len(pert[0])))
+
+    if GUI:
+        data_processor_GUI(working_perts, max_cardinality, oscillation, phenotype, controllable)
+    else:
+        data_processor(working_perts, pert_graph.unit_colors().cardinality())
+
+control_machine(PATH, PHENOTYPE, CONTROLLABLE, OSCILLATION, True)
